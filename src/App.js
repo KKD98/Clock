42bca1c3208245ebeed1fd84efb216b0ce84cbb4
KKD98 <KajolKona98@gmail.com>
2021-10-01 01:47:37 +0600

    Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Digitalclock from './mycomponent/Digitalclock';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Digitalclock></Digitalclock>
+    </div>
+  );
+}
+
+export default App;
